d31dfec38c4b7c2812021f081347c9adb84b552c
Damian <themrdamus@gmail.com>
2018-05-20 20:04:40 +0100

    database initialized

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://admin:kitkat@ds129770.mlab.com:29770/stock-trading'
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  
+  MongoClient.connect(url, (err, client) => {
+    db = client.db('stock-trading')
+    db.collection('users')
+      .find()
+      .toArray(function(err, results) {
+        console.log(results)
+        res.send(results);
+      })
+    console.log('err', err);
+  })
+})
 
 module.exports = router;
+
