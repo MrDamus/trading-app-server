8afa9dca185a1753eabc73fda04473e1d7487ced
Damian <themrdamus@gmail.com>
2018-05-25 22:35:52 +0100

    updating user wallet

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,26 @@ router.post('/:email', function (req, res, next) {
   })
 });
 
+router.put('/:email', function (req, res, next) {
+  MongoClient.connect(url, (err, client) => {
+    console.log(req.body.money, req.body.wallet)
+    db = client.db('stock-trading')
+    db.collection('users')
+      .findOneAndUpdate({ email: req.params.email },
+      {
+        $inc: {
+          money: -req.body.money,
+        },
+        $push: {
+          wallet: req.body.wallet
+        }
+      },{}, (err, result) => {
+        console.warn(result)
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  });
+})
 
 router.post('/', (req, res) => {
   MongoClient.connect(url, (err, client) => {
@@ -41,7 +61,6 @@ router.post('/', (req, res) => {
     db.collection('users')
       .find({ email: newUser.email })
       .toArray(function (err, users) {
-          console.log(users)
           if (users && users.length > 0) {
             // error 
             res.status(500).send({ error: 'User already exists' });
@@ -49,7 +68,7 @@ router.post('/', (req, res) => {
             db.collection('users')
               .save(newUser, (err, result) => {
                 if (err) return console.log(err)
-                console.log('saved to database')
+                console.log('Your account has been created.')
                 res.redirect('/')
               })
           }
@@ -57,23 +76,7 @@ router.post('/', (req, res) => {
   })
 })
 
-// /* GET users listing. */
-// router.put('/', (req, res) => {
-//   // Handle put request
-//   })
 
-// router.post('/', (req, res) => {
-//   MongoClient.connect(url, (err, client) => {
-//     db = client.db('stock-trading')
-//     const newUser = {...req.body, wallet: [], money: 1000}
-//    db.collection('users').save(newUser, (err, result) => {
-//     if (err) return console.log(err)
-
-//     console.log('saved to database')
-//     res.redirect('/')
-//   })
-//   })
-// })
 
 // Clearing database
 
@@ -87,86 +90,5 @@ router.delete('/', (req, res) => {
     }
   })
 })
-
-// https://stackoverflow.com/questions/42381683/how-to-check-if-user-with-email-already-exists?rq=1
-
-// router.post('/register', [
-//   check('email')
-//   .not()
-//   .isEmpty()
-//   .withMessage('Email is required')
-//   .isEmail()
-//   .withMessage('Invalid Email')
-//   .custom((value, {req}) => {
-//     return new Promise((resolve, reject) => {
-//       db = client.db('stock-trading')
-//       db.collection('users').findOne({email:req.body.email}, function(err, user){
-//         if(err) {
-//           reject(new Error('Server Error'))
-//         }
-//         if(Boolean(user)) {
-//           reject(new Error('E-mail already in use'))
-//         }
-//         resolve(true)
-//       });
-//     });
-//   }),
-//   // Check Password
-//   check('password')
-//   .not()
-//   .isEmpty()
-//   .withMessage('Password is required'),
-//   // Check Password Confirmation
-//   check('confirmedPassword', 'Passwords do not match')
-//   .exists()
-//   .custom((value, { req }) => value === req.body.password)
-// ], function(req, res) {
-//   var name = req.body.name;
-//   var email = req.body.email;
-//   var password = req.body.password;
-//   var confirmedPassword = req.body.confirmedPassword;
-
-//   // Check for Errors
-//   const validationErrors = validationResult(req);
-//   let errors = [];
-//   if(!validationErrors.isEmpty()) {
-//     Object.keys(validationErrors.mapped()).forEach(field => {
-//       errors.push(validationErrors.mapped()[field]['msg']);
-//     });
-//   }
-
-//   if(errors.length){
-//     res.render('register',{
-//       errors:errors
-//     });
-//   }  else {
-//     var newUser = new User({
-//       name: name,
-//       email: email,
-//       password: password,
-//       admin: false,
-//       active: false
-//     });
-
-//     User.createUser(newUser, function (err, user) {
-//       if (err) {
-//         throw err;
-//       }
-//     });
-
-//     req.flash('success_msg', 'You are registerd and can now login');
-//     res.redirect('/users/login');
-//   }})
-
-// MongoClient.connect(url, function(err, db) {
-//   if (err) throw err;
-//   var dbo = db.db("stock-trading");
-//   // var myquery = { address: /^O/ };
-//   dbo.collection("users").drop({}, function(err, obj) {
-//     if (err) throw err;
-//     console.log(obj.result.n + " document(s) deleted");
-//     db.close();
-//   });
-// });
 
 module.exports = router;
